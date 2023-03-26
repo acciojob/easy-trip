3f5f594269f36a44234806a37496c97341da6a6d
Tushar <tushargarg0110@gmail.com>
2023-03-26 14:49:28 +0530

    Ease My Trip solution added

--- src/main/java/com/driver/EaseMyTrip.java
+++ src/main/java/com/driver/EaseMyTrip.java
@@ -3,14 +3,11 @@ package com.driver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @SpringBootApplication
-public class Vpn {
+public class EaseMyTrip {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Vpn.class, args);
+		SpringApplication.run(EaseMyTrip.class, args);
 	}
 
 }
