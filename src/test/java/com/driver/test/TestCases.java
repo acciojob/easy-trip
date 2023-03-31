4387a612fe252ab3c488332b1844132ce3e9f7e8
Tushar <tushargarg0110@gmail.com>
2023-04-01 03:46:14 +0530

    Airport Controller Autowired in Testcases

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.*;
@@ -16,7 +17,10 @@ import java.util.*;
 @SpringBootTest(classes = EaseMyTrip.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestCases {
-    AirportController airportController = new AirportController();
+
+    @Autowired
+    AirportController airportController;
+
     @BeforeEach
     public void setup(){
         airportController.addAirport(new Airport("IGI",2,City.DELHI));
